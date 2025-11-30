8bc6962330e335662975bd7e07986d9771e61282
Sujal Vasara <vasarasujal.cg@gmail.com>
2025-11-30 12:24:22 +0530

    Change favicon icon.

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,19 +14,15 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: "/icon-light-32x32.png",
-        media: "(prefers-color-scheme: light)",
-      },
-      {
-        url: "/icon-dark-32x32.png",
-        media: "(prefers-color-scheme: dark)",
+        url: "/favicon.ico",
+        sizes: "any",
       },
       {
         url: "/icon.svg",
         type: "image/svg+xml",
       },
     ],
-    apple: "/apple-icon.png",
+    apple: "/apple-touch-icon.png",
   },
 }
 
